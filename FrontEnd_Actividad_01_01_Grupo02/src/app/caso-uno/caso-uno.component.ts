f65309189e730351bd6f02660ebeb66e5f8e193e
Daniel Patiño <57640988+Daniel070999@users.noreply.github.com>
2023-09-17 12:28:34 -0500

    Controles de formulario

--- FrontEnd_Actividad_01_01_Grupo02/src/app/caso-uno/caso-uno.component.ts
+++ FrontEnd_Actividad_01_01_Grupo02/src/app/caso-uno/caso-uno.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+interface TipoVehiculo {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-caso-uno',
   templateUrl: './caso-uno.component.html',
   styleUrls: ['./caso-uno.component.css']
 })
+
+
+
 export class CasoUnoComponent {
 
   constructor(private service: DataService) { }
@@ -15,14 +23,47 @@ export class CasoUnoComponent {
   anio: any = '';
   marca: any = '';
   modelo: any = '';
+  tipo: any = '';
 
-  vehiculos: any = [{ placa: 'placa asd', color:'amarillo'}];
+  tiposVehiculo: TipoVehiculo[] = [
+    { value: 'AUTO', viewValue: 'Auto' },
+    { value: 'CAMIONETA', viewValue: 'Camioneta' },
+    { value: 'CAMION', viewValue: 'Camion' },
+  ];
+
+  vehiculos: any = [];
+
+  verificar(): boolean {
+    let validateForm: boolean = false;
+    if (this.placa != '' && this.color != '' && this.anio != '' && this.marca != '' && this.modelo != '' && this.tipo != '') {
+      validateForm = true;
+    }
+    return validateForm;
+  }
 
   ok() {
-    console.log(this.placa);
+
+    if (this.verificar()) {
+      const data = {
+        placa: this.placa,
+        color: this.color,
+        anio: this.anio,
+        marca: this.marca,
+        modelo: this.modelo,
+        tipo: this.tipo
+      }
+      this.service.postVehiculo(data).subscribe(response => {
+        this.recargar();
+      });
+    } else {
+      alert('Debe llenar todos los campos');
+    }
+
   }
   recargar() {
-    
+    this.service.getVehiculo().subscribe(response => {
+      this.vehiculos = response;
+    });
   }
 
 }
