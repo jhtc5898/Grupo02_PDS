04d1341ea7ee8398815a3b31408c6314277f75e3
JohnTenesaca <jtenesaca@ec.krugercorp.com>
2023-09-16 20:52:18 -0500

    Manejo List

--- BackEnd_Actividad_01_01_Grupo02/src/main/java/ec/edu/ups/backend_actividad_01_01_grupo02/escenario02/controlador/ActivoFijoController.java
+++ BackEnd_Actividad_01_01_Grupo02/src/main/java/ec/edu/ups/backend_actividad_01_01_grupo02/escenario02/controlador/ActivoFijoController.java
@@ -6,12 +6,15 @@ import ec.edu.ups.backend_actividad_01_01_grupo02.escenario02.model.ActivoFijo;
 import ec.edu.ups.backend_actividad_01_01_grupo02.escenario02.servicio.ActivoFijoBuilder;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/activofijo")
+@CrossOrigin
 public class ActivoFijoController {
 
     List<ActivoFijo> activoFijoBuilders = new ArrayList<>();
@@ -27,10 +30,9 @@ public class ActivoFijoController {
 
 
     @GetMapping("getactivofijo")
-    public String getactivofijo(){
-//        var buil = new ActivoFijoBuilder();
-//        var activoFijo =  buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build();
-        return activoFijoBuilders.toString();
+    @ResponseBody
+    public List<ActivoFijo>  getactivofijo(){
+        return activoFijoBuilders;
     }
 
 }
