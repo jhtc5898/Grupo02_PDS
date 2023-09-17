e281537122906126a34876fb50662e1c4da6837d
JohnTenesaca <jtenesaca@ec.krugercorp.com>
2023-09-16 20:14:17 -0500

    Manejo List

--- BackEnd_Actividad_01_01_Grupo02/src/main/java/ec/edu/ups/backend_actividad_01_01_grupo02/escenario02/controlador/ActivoFijoController.java
+++ BackEnd_Actividad_01_01_Grupo02/src/main/java/ec/edu/ups/backend_actividad_01_01_grupo02/escenario02/controlador/ActivoFijoController.java
@@ -1,6 +1,10 @@
 package ec.edu.ups.backend_actividad_01_01_grupo02.escenario02.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+import ec.edu.ups.backend_actividad_01_01_grupo02.escenario02.model.ActivoFijo;
 import ec.edu.ups.backend_actividad_01_01_grupo02.escenario02.servicio.ActivoFijoBuilder;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/activofijo")
 public class ActivoFijoController {
+
+    List<ActivoFijo> activoFijoBuilders = new ArrayList<>();
+    @PostConstruct
+    public void init(){
+
+        var buil = new ActivoFijoBuilder();
+        activoFijoBuilders.add(buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build());
+        activoFijoBuilders.add(buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build());
+        activoFijoBuilders.add(buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build());
+
+    }
+
+
     @GetMapping("getactivofijo")
     public String getactivofijo(){
-        var buil = new ActivoFijoBuilder();
-        var activoFijo =  buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build();
-        return activoFijo.toString();
+//        var buil = new ActivoFijoBuilder();
+//        var activoFijo =  buil.conColor("azul").conNombre("Chevrolet").conPrecio(1.235).build();
+        return activoFijoBuilders.toString();
     }
 
 }
