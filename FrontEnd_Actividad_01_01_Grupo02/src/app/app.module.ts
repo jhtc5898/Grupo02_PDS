6e8441372466c68cddca2f1bfbb83e96d99482c1
jhtc5898 <33676340+jhtc5898@users.noreply.github.com>
2023-09-16 21:00:13 -0500

    Add files via upload

--- FrontEnd_Actividad_01_01_Grupo02/src/app/app.module.ts
+++ FrontEnd_Actividad_01_01_Grupo02/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CasoUnoComponent } from './caso-uno/caso-uno.component';
 import { CasoDosComponent } from './caso-dos/caso-dos.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
