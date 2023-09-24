5dff7d59c26110ffb61223ae20ebe8db125ba9e8
Daniel070999 <danielixo99@gmail.com>
2023-09-23 22:19:27 -0500

    Arreglo de guardar el campo temperatura C o F

--- BackEnd_Actividad_02_01_Grupo02/src/main/java/com/grupo02/grupo02/escenario02/controller/Escenario2Controller.java
+++ BackEnd_Actividad_02_01_Grupo02/src/main/java/com/grupo02/grupo02/escenario02/controller/Escenario2Controller.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/escenario02")
-//@CrossOrigin
+@CrossOrigin
 public class Escenario2Controller {
 
     @Autowired
--- BackEnd_Actividad_02_01_Grupo02/src/main/java/com/grupo02/grupo02/escenario02/service/Escenario2Service.java
+++ BackEnd_Actividad_02_01_Grupo02/src/main/java/com/grupo02/grupo02/escenario02/service/Escenario2Service.java
@@ -16,21 +16,21 @@ public class Escenario2Service {
     Escenario2Repository escenario2Repository;
     public double guardarTemperatura(TemperaturaDTO temperaturaDTO){
         var registro = new TemperaturaDTO();
-        registro.setTemperatura(temperaturaDTO.getTemperatura());
         registro.setEdificio(temperaturaDTO.getEdificio());
         registro.setTipo(temperaturaDTO.getTipo());
         registro.setPiso(temperaturaDTO.getPiso());
-        escenario2Repository.add(registro);
         double temp = 0;
         if(temperaturaDTO.getTipo().equalsIgnoreCase("F")){
             temp = new AdapterFanhrenheit().temperatura(temperaturaDTO.getTemperatura());
+            registro.setTemperatura(temp);
         }
         if(temperaturaDTO.getTipo().equalsIgnoreCase("C")){
             temp = new AdapterCelsius().temperatura(temperaturaDTO.getTemperatura());
+            registro.setTemperatura(temp);
         }
+        escenario2Repository.add(registro);
         return temp;
     }
-
     public List<TemperaturaDTO> registroList(){
         return escenario2Repository.getAll();
     }
