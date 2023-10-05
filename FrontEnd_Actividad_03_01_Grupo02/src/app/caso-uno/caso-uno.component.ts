2bdbc5c952fcf8da13fbea5c1c27d1b812680265
Daniel Patiño <57640988+Daniel070999@users.noreply.github.com>
2023-10-04 20:21:11 -0500

    front del escenario 1

--- FrontEnd_Actividad_03_01_Grupo02/src/app/caso-uno/caso-uno.component.ts
+++ FrontEnd_Actividad_03_01_Grupo02/src/app/caso-uno/caso-uno.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-caso-uno',
@@ -7,4 +8,82 @@ import { Component } from '@angular/core';
 })
 export class CasoUnoComponent {
 
+  constructor(private service: DataService) { }
+
+  chatjuan: any = '';
+  chatpedro: any = '';
+
+  msg: any = [];
+
+
+
+  verMsg() {
+    this.service.verMsg().subscribe({
+      next: response => {
+        this.msg = response;
+        this.clearChat();
+      }, error: err => {
+        console.log(err);
+
+      }
+    });
+  }
+
+  msgJuan() {
+    const chatOrigen = 'Juan';
+    const chatDestino = 'Pedro';
+    const msg = this.chatjuan;
+    if (msg) {
+      const data = {
+        userNameFrom: chatOrigen,
+        userNameTo: chatDestino,
+        message: msg
+      }
+      this.service.enviarMsg(data).subscribe({
+        next: response => {
+          console.log(response);
+
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+      setTimeout(() => {
+        this.verMsg()
+      }, 500);
+    } else {
+      alert('no hay texto para enviar');
+    }
+  }
+
+  msgPedro() {
+    const chatOrigen = 'Pedro';
+    const chatDestino = 'Juan';
+    const msg = this.chatpedro;
+    if (msg) {
+      const data = {
+        userNameFrom: chatOrigen,
+        userNameTo: chatDestino,
+        message: msg
+      }
+      this.service.enviarMsg(data).subscribe({
+        next: response => {
+          console.log(response);
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+      setTimeout(() => {
+        this.verMsg()
+      }, 500);
+
+    } else {
+      alert('no hay texto para enviar');
+    }
+  }
+  clearChat() {
+    this.chatjuan = '';
+    this.chatpedro = '';
+  }
 }
--- FrontEnd_Actividad_03_01_Grupo02/src/app/services/data.service.ts
+++ FrontEnd_Actividad_03_01_Grupo02/src/app/services/data.service.ts
@@ -8,16 +8,25 @@ export class DataService {
   private apiUrl1 = 'http://localhost:8080/interpreter/evaluarFecha'; // Define la URL del servicio
   private apiUrl2 = 'http://localhost:8080/interpreter/evaluarGeneral'; // Define la URL del servicio
 
+  private apiUrlMsg1 = 'http://localhost:8080/mediator/sendmessage';
+  private apiUrlMsg2 = 'http://localhost:8080/mediator/chatlist';
+
   constructor(private http: HttpClient) { }
 
   evaluarFecha(param: any) {
     // Aquí puedes hacer una petición HTTP
-    return this.http.get(this.apiUrl1 +"?expresionEvaluar="+param);
+    return this.http.get(this.apiUrl1 + "?expresionEvaluar=" + param);
   }
 
   evaluarGeneral(param: any) {
     // Aquí puedes hacer una petición HTTP
-    return this.http.get(this.apiUrl2 +"?expresionEvaluar="+param);
+    return this.http.get(this.apiUrl2 + "?expresionEvaluar=" + param);
   }
 
+  enviarMsg(data: any) {
+    return this.http.post(this.apiUrlMsg1, data);
+  }
+  verMsg() {
+    return this.http.get(this.apiUrlMsg2);
+  }
 }
